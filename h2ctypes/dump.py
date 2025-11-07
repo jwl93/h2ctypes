21ebf9f50c0fe52b49c7808d0d6223574ffac6e8
jwl <408066557@qq.com>
2025-11-07 11:09:21 +0800

    bugfix

--- h2ctypes/dump.py
+++ h2ctypes/dump.py
@@ -27,22 +27,28 @@ def get_struct_str(filepath):
         context = ''
     return context, type_defs
 
-def parse_str2json(context):
+def parse_str2json(context, verbose=False):
     structs = {}
     enums = {}
     parsing = None
     parsing_type = None
     parse_end_counter = 1
-    name_pattern = re.compile(r'(struct|enum)\s+(\w+)\s*\{*')
-    kv_pattern = re.compile(r'([\w\*]+)\s+(\w+)\s*;*')
+    name_pattern = re.compile(r'(struct|enum)\s+(\w+)\s*\{*(.*)')
+    kv_pattern = re.compile(r'([\w\*]+)\s+([\w\[\]]+)\s*;*')
     for line in context.split('\n'):
+        if parse_end_counter == 0:
+            parsing = None
+            parsing_type = None
         cline = line.strip()
+        if verbose:
+            print(cline)
         if not cline:
             continue
         if 'struct ' in cline or 'enum ' in cline:
             assert not parsing, f'parsing failed! {cline}'
             parse_end_counter = 1
-            name = name_pattern.search(cline).group(2)
+            matched = name_pattern.search(cline)
+            name = matched.group(2)
             parsing = name
             if 'struct ' in cline:
                 parsing_type = 'struct'
@@ -50,40 +56,60 @@ def parse_str2json(context):
             else:
                 parsing_type = 'enum'
                 enums[parsing] = OrderedDict()
-            #print(f'start parsing struct|enum: {name}...')
-        else:
-            cline = cline.strip('{ ')
-            if 'union' in cline:
-                cline = cline.replace('union', '').strip()
-                parse_end_counter += 1
-            if not cline:
-                continue
-            if '}' in cline:
-                assert 'struct' not in cline and 'enum' not in cline, cline
-                parse_end_counter -= 1
-                cline = ''
-            if parse_end_counter == 0:
-                parsing = None
-                parsing_type = None
-            if not cline:
-                continue
-            if parsing_type == 'struct':
-                kv_match = kv_pattern.search(cline)
+            if verbose:
+                print(f'start parsing {parsing_type}: {name}...')
+            cline = matched.group(3)
+        cline = cline.strip('{ ')
+        if 'union' in cline:
+            cline = cline.replace('union', '').strip()
+            parse_end_counter += 1
+        if not cline:
+            continue
+        if '}' in cline:
+            assert 'struct' not in cline and 'enum' not in cline, cline
+            parse_end_counter -= 1
+            cline = cline.split('}', 1)[0].strip()
+        if not cline:
+            continue
+        if verbose:
+            print(cline)
+        if parsing_type == 'struct':
+            split_clines = cline.split(';')
+            for split_cline in split_clines:
+                split_cline = split_cline.strip()
+                if not split_cline:
+                    continue
+                kv_match = kv_pattern.search(split_cline)
                 k = kv_match.group(1)
                 v = kv_match.group(2)
-                structs[parsing][v] = k
-            else:
-                enums[parsing][1] = cline
-            assert 'struct ' not in cline and 'enum ' not in cline, cline
+                length = None
+                if '[' in v:
+                    v_split = v.split('[')
+                    v = v_split[0]
+                    length = 1
+                    for v_len in v_split[1:]:
+                        length *= int(v_len.strip('] '))
+                    print(parsing, k, v, length)
+                structs[parsing][v] = { 'type': k, 'length': length }
+        else:
+            split_clines = cline.split(',')
+            for split_cline in split_clines:
+                split_cline = split_cline.strip()
+                if not split_cline:
+                    continue
+                if verbose:
+                    print(split_cline)
+                enums[parsing][split_cline] = {'type': 'enum'} # only record info, enum value is not so import here.
+        assert 'struct ' not in cline and 'enum ' not in cline, cline
     return structs, enums
 
-def load_structs(filepaths):
+def load_structs(filepaths, verbose=False):
     result_structs = {}
     result_enums = {}
     result_type_defs = {}
     for filepath in filepaths:
         struct_context, type_defs = get_struct_str(filepath)
-        structs, enums = parse_str2json(struct_context)
+        structs, enums = parse_str2json(struct_context, verbose=verbose)
         result_structs.update(structs)
         result_enums.update(enums)
         result_type_defs.update(type_defs)
@@ -92,16 +118,17 @@ def load_structs(filepaths):
 
 def parse2dict(item, base_infos, ret=OrderedDict()):
     structs, enums, type_defs = base_infos
-    for k, v in item.items():
+    for k, k_item in item.items():
+        v = k_item['type']
         if v in enums:
-            ret[k] = 'uint32_t'
+            ret[k] = {'type': 'uint32_t', 'length': None}
         elif v in structs:
             ext_ret = parse2dict(structs[v], base_infos)
             ret.update(ext_ret)
         elif v in type_defs:
-            ret[k] = type_defs[v]
+            ret[k] = {'type': type_defs[v], 'length': None}
         else:
-            ret[k]= v
+            ret[k] = k_item
     return ret
 
 @app.command()
@@ -111,9 +138,10 @@ def dump(dump_structname: str,
         dump_py_path: str = '{dump_structname}.py',
         noconvert: bool = False,
         pack: int = 8,
+        verbose: bool = False,
     ):
     import json
-    base_infos = load_structs(input_header_files)
+    base_infos = load_structs(input_header_files, verbose=verbose)
     structs, enums, type_defs = base_infos
     return_dict = parse2dict(structs[dump_structname], base_infos)
     if noconvert:
@@ -133,7 +161,7 @@ def dump(dump_structname: str,
         res = template.render({
             'struct_name': dump_structname,
             'pack': pack,
-            'field_infos': [{'name': k, 'ctypes': _CXX2CTYPES[v]} for k, v in return_dict.items()],
+            'field_infos': [{'name': k, 'ctypes': _CXX2CTYPES[v['type']], 'length': v['length']} for k, v in return_dict.items()],
         })
         dump_py_path = dump_py_path.format(**locals())
         with open(dump_py_path, 'w') as f:
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,8 +5,9 @@ import json
 def test_parse():
     test_root = os.path.dirname(os.path.abspath(__file__))
     test_file = os.path.join(test_root, 'basic.h')
-    base_infos = load_structs([test_file,])
+    base_infos = load_structs([test_file,], verbose=False)
     structs, enums, type_defs = base_infos
-    return_dict = parse2dict(structs[dump_structname], base_infos)
+    print(structs['NestedStruct'])
+    return_dict = parse2dict(structs['NestedStruct'], base_infos)
     print(return_dict)
     assert True
